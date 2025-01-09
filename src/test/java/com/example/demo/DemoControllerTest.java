3911c8bd9d0e40a8255b6305b02481ecae5fe3d8
Gustav Hagland <gustav.hagland@vermiculus.se>
2025-01-09 10:32:57 +0100

    Add example with WebTestClient

--- src/test/java/com/example/demo/DemoControllerTest.java
+++ src/test/java/com/example/demo/DemoControllerTest.java
@@ -1,20 +1,20 @@
 package com.example.demo;
 
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.test.web.servlet.client.MockMvcWebTestClient;
 
 import java.time.LocalDate;
+import java.util.stream.Stream;
 
 class DemoControllerTest {
 
-    @Test
-    void mvcTest() {
-        var client = MockMvcWebTestClient.bindToController(new DemoController())
-                .configureClient()
-                .build();
-
+    @ParameterizedTest
+    @MethodSource("clients")
+    void mvcTest(WebTestClient client) {
         var res = client.get()
                 .uri("/date")
                 .accept(MediaType.APPLICATION_JSON)
@@ -24,5 +24,13 @@ class DemoControllerTest {
                 .getResponseBody();
 
         Assertions.assertThat(res).isNull();
+    }
+
+    public static Stream<WebTestClient> clients() {
+        return Stream.of(MockMvcWebTestClient.bindToController(new DemoController())
+                .configureClient()
+                .build(), WebTestClient.bindToController(new DemoController())
+                .configureClient()
+                .build());
     }
 }
